380aaf9393ba7c9a6768d76f5c22528b90e8cbdf
Lucas Oliveira <midasmatrix123@gmail.com>
2020-12-28 18:32:39 -0300

    problem 1866 complete

---
+++ 1866.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int c, s, r;
+    cin >> c;
+    while (c > 0)
+    {
+        cin >> s;
+        cout << s%2 << "\n"; 
+        c--;
+    }
+    
+}
